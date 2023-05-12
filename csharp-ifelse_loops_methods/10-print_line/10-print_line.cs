eeec56288985b2e2c7d96c17a853077540f68ecc
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 18:31:15 -0500

    10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length > 0)
+	{
+		Console.Write(new String('_', length));
+	}
+	Console.WriteLine();
+    }
+}
--- csharp-ifelse_loops_methods/main.sh
+++ csharp-ifelse_loops_methods/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 9-add
-dotnet build "9-add"
+dotnet new console -o 10-print_line
+dotnet build "10-print_line"
