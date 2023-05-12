fd3ea2403a13acf2b97a8ae98bbadf694ad18aa1
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 18:43:39 -0500

    11

--- csharp-ifelse_loops_methods/main.sh
+++ csharp-ifelse_loops_methods/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 10-print_line
-dotnet build "10-print_line"
+dotnet new console -o 11-print_diagonal
+dotnet build "11-print_diagonal"
