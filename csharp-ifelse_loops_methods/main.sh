359ae10c761670fe1d7e419ba0c151b85f9472cc
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 17:50:36 -0500

    1

--- csharp-ifelse_loops_methods/main.sh
+++ csharp-ifelse_loops_methods/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 0-positive_or_negative
-dotnet build "0-positive_or_negative"
+dotnet new console -o 1-last_digit
+dotnet build "1-last_digit"
