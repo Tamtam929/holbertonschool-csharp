e3e48e20b4a0efc40a7b6914ff372be8fa783750
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 14:15:00 -0500

    6

--- csharp-hello_world/uwu.sh
+++ csharp-hello_world/uwu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 5-print_float
-dotnet build "5-print_float"
+dotnet new console -o 6-print_string
+dotnet build "6-print_string"
