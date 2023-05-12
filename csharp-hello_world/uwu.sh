385f44012ed0e34ba685ba3eab612463d637e66c
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 16:41:00 -0500

    11

--- csharp-hello_world/uwu.sh
+++ csharp-hello_world/uwu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 10-copy_cut_paste
-dotnet build "10-copy_cut_paste"
+dotnet new console -o 11-concat_edges
+dotnet build "11-concat_edges"
