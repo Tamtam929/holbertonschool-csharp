2e5e3101926d9da46c8b8ddcbc9c0177660f4d93
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 19:54:59 -0500

    5

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 4-print_list
-dotnet build "4-print_list"
+dotnet new console -o 5-max_integer
+dotnet build "5-max_integer"
