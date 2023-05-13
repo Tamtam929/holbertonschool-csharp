ac36a3b4aada5cbd572c766e1f26da1bdcfc5e3b
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 20:24:52 -0500

    10

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 9-add_key_value
-dotnet build "9-add_key_value"
+dotnet new console -o 10-delete_key_value
+dotnet build "10-delete_key_value"
