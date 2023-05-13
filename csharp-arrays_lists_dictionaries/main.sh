bf3fc11e434b5482cd31310ee6d8dc8382b5180e
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 20:44:04 -0500

    15

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 14-rectangular_array
-dotnet build "14-rectangular_array"
+dotnet new console -o 15-square_matrix
+dotnet build "15-square_matrix"
