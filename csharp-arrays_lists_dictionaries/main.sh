94fe2ec6d76d09827b3284e19cc3f04ed4ede500
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 20:40:16 -0500

    14

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 13-best_score
-dotnet build "13-best_score"
+dotnet new console -o 14-rectangular_array
+dotnet build "14-rectangular_array"
