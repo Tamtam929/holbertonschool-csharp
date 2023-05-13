4ec844bad7cf7e22713e4cf283e0bb6f6750f4d2
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 20:37:09 -0500

    13

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 12-print_sorted_dictionary
-dotnet build "12-print_sorted_dictionary"
+dotnet new console -o 13-best_score
+dotnet build "13-best_score"
