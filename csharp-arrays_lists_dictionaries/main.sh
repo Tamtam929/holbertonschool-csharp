50f09393e4a200d0c5b154d1748ec4b06e7b5c5a
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 20:32:05 -0500

    12

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 11-multiply_by_2
-dotnet build "11-multiply_by_2"
+dotnet new console -o 12-print_sorted_dictionary
+dotnet build "12-print_sorted_dictionary"
