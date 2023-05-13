b50622e405a35317cae8eee16175daf502d82d38
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 19:38:31 -0500

    2

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 1-element_at
-dotnet build "1-element_at"
+dotnet new console -o 2-replace_element
+dotnet build "2-replace_element"
