abb21775fe8e94c777db649db9d46478ed586627
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 21:26:36 -0500

    7

--- csharp-hashset_stack_queue_linkedlist/main.sh
+++ csharp-hashset_stack_queue_linkedlist/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 6-linkedlist_length
-dotnet build "6-linkedlist_length"
+dotnet new console -o 7-linkedlist_add
+dotnet build "7-linkedlist_add"
