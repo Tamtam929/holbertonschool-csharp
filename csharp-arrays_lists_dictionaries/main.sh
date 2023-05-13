60797369bb6a7ea9447e1b353da7a87c9d700850
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-12 19:50:06 -0500

    4

--- csharp-arrays_lists_dictionaries/main.sh
+++ csharp-arrays_lists_dictionaries/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 3-print_array_reverse
-dotnet build "3-print_array_reverse"
+dotnet new console -o 4-print_list
+dotnet build "4-print_list"
