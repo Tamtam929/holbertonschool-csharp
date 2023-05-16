97222058a18e2fdb145e745bd3892e164c02fa1d
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-16 12:10:31 -0500

    1

--- csharp-exceptions/main.sh
+++ csharp-exceptions/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 0-safe_list_print
-dotnet build "0-safe_list_print"
+dotnet new console -o 1-divide_print
+dotnet build "1-divide_print"
