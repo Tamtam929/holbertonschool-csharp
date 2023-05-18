2bc776a65698ba86a55b5e38e3b8562167880d9b
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-18 14:20:57 -0500

    2

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,9 +1,36 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
 
-public class Except
+public class List
 {
-    public static void Throw()
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        throw new Exception();
+        List<int> sum = new List<int>();
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                sum.Add(div(list1[i], list2[i]));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        return sum;
+    }
+    static int div(int a, int b)
+    {
+        int sum = 0;
+        try
+        {
+            sum = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            sum = 0;
+        }
+        return sum;
     }
 }
--- csharp-exceptions/main.sh
+++ csharp-exceptions/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 2-divide_lists
-dotnet build "2-divide_lists"
+dotnet new console -o 3-throw_exception
+dotnet build "3-throw_exception"
