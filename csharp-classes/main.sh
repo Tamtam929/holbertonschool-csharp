b9be8502ff4682f7a67433000af54e73f7c8251f
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-20 12:13:18 -0500

    3

--- csharp-classes/main.sh
+++ csharp-classes/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 2-enemy
-dotnet build "2-enemy"
+dotnet new console -o 3-enemy
+dotnet build "3-enemy"
