07562aba84e514115a55de55a09e66d7dba2970d
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-20 10:29:22 -0500

    4

--- csharp-exceptions/main.sh
+++ csharp-exceptions/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 3-throw_exception
-dotnet build "3-throw_exception"
+dotnet new console -o 4-throw_exception_msg
+dotnet build "4-throw_exception_msg"
