870ef6291357fe5368eeeaa3ff8be4487ea72673
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-23 11:56:14 -0500

    1

--- csharp-inheritance/main.sh
+++ csharp-inheritance/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 0-is
-dotnet build "0-is"
+dotnet new console -o 1-instance
+dotnet build "1-instance"
