6b3b582f7c202acbf1ac9fb0c3cabc60f80fa05d
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 16:43:43 -0500

    5

---
+++ csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+/// <summary>Generic class</summary>
+class Queue<T>
+{
+    Node head = null;
+    Node tail = null;
+    int count = 0;
+
+    /// <summary>Returns type of Queue object</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    public int Count()
+    {
+        return this.count;
+    }
+    /// <summary>Creates a Node</summary>
+    public void Enqueue(T value)
+    {
+        this.count += 1;
+        if (count == 1)
+        {
+            this.head = new Node(value);
+            this.tail = head;
+        } else {
+            this.tail.next = new Node(value);
+            this.tail = this.tail.next;
+        }
+    }
+
+    /// <summary>Removes a Node</summary>
+    public T Dequeue()
+    {
+        if (this.count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        } else {
+            T value = head.value;
+            head = head.next;
+            count -= 1;
+            return value;
+        }
+    }
+
+    /// <summary>Looks at the head value</summary>
+    public T Peek()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        } else {
+            return head.value;
+        }
+    }
+
+    /// <summary>Prints values at all nodes</summary>
+    public void Print()
+    {
+        if (count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+        {
+            Node skimmer = head;
+            while (skimmer != null)
+            {
+                Console.WriteLine(skimmer.value);
+                skimmer = skimmer.next;
+            }
+        }
+    }
+
+    /// <summary>Concatonates queue</summary>
+    public string Concatenate()
+    {
+        if (count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return null;
+        }
+        if (typeof(T) != typeof(String) && typeof(T) != typeof(Char))
+        {
+            Console.WriteLine("Concatenate() is for a queue of Strings or Chars only");
+            return null;
+        }
+        Node skimmer = head;
+        string concat = "";
+        while (skimmer != null)
+        {
+            concat += skimmer.value;
+            if (typeof(T) == typeof(String) && skimmer.next != null)
+                concat += " ";
+            skimmer = skimmer.next;
+        }
+        return concat;
+    }
+
+    /// <summary>Nodes for queuing</summary>
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+        public Node (T value)
+        {
+            this.value = value;
+        }
+    }
+}
--- csharp-generics/main.sh
+++ csharp-generics/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 4-print
-dotnet build "4-print"
+dotnet new console -o 5-concatenate
+dotnet build "5-concatenate"
