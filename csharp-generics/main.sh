2088a5607767fda7263486fd60575eb10d79a47a
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 16:06:16 -0500

    2

--- csharp-generics/main.sh
+++ csharp-generics/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 1-enqueue
-dotnet build "1-enqueue"
+dotnet new console -o 2-dequeue
+dotnet build "2-dequeue"
