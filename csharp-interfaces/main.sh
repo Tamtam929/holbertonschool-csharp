3ab28aea78cb5123950ca34a3c38cba19310eae2
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 17:45:40 -0500

    4

--- csharp-interfaces/main.sh
+++ csharp-interfaces/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 3-decorations
-dotnet build "3-decorations"
+dotnet new console -o 4-keys
+dotnet build "4-keys"
