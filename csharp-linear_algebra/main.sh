005d66d5d6dd713cee531de6ed899c0c8f1c33ff
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 09:46:32 -0500

    30

--- csharp-linear_algebra/main.sh
+++ csharp-linear_algebra/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 20-matrix_rotate_2D
-dotnet build "20-matrix_rotate_2D"
+dotnet new console -o 33-inverse_2D
+dotnet build "33-inverse_2D"
