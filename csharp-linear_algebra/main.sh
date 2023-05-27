e3bfe392a80ec87bf0eae3a69f1e37ddcd4d4032
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 09:07:30 -0500

    12

--- csharp-linear_algebra/main.sh
+++ csharp-linear_algebra/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 9-vector_scalar_mul
-dotnet build "9-vector_scalar_mul"
+dotnet new console -o 12-dot_product
+dotnet build "12-dot_product"
