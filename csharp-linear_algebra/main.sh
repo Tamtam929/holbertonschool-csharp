da662dfda29f6482f52f70b1523a11fa56d07262
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 09:12:30 -0500

    14

--- csharp-linear_algebra/main.sh
+++ csharp-linear_algebra/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 12-dot_product
-dotnet build "12-dot_product"
+dotnet new console -o 14-matrix_addition
+dotnet build "14-matrix_addition"
