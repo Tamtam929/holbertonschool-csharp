01f442ab5fd71ed80cb8f53401c007fa6b879696
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-27 15:49:18 -0500

    0

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,14 +1,7 @@
 using System;
 
-/// <summary>
-/// Class called Queue.
-///</summary>
 class Queue<T>
 {
-    /// <summary>
-    /// Queue should not inherit from other classes or interfaces.
-    /// </summary>
-    /// <return> returns the Queue’s type.</return>
     public Type CheckType()
     {
         return typeof(T);
