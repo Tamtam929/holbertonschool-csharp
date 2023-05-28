5660edc8a64c490f921a1576389f6c34f084da0d
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-05-28 09:11:17 -0500

    0

--- csharp-delegates_events/main.sh
+++ csharp-delegates_events/main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # initializes, builds, and runs a C# project in a folder
-dotnet new console -o 4-throw_exception_msg
-dotnet build "4-throw_exception_msg"
+dotnet new console -o 0-universal_health
+dotnet build "0-universal_health"
